40f9fc47efce34dbfb6f85b01e6f9eff612be549
Sheikh Jebran <sheikhjebran@gmail.com>
2025-09-11 23:38:20 +0530

    test6

--- src/app/api/products/[productId]/route.ts
+++ src/app/api/products/[productId]/route.ts
@@ -1,12 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getProductById } from '@/services/server/productServerService';
 
+type Props = {
+    params: {
+        productId: string;
+    };
+    searchParams: { [key: string]: string | string[] | undefined };
+};
+
 export async function GET(
     request: NextRequest,
-    { params }: { params: { productId: string } }
+    props: Props
 ) {
+    const { productId } = props.params;
     try {
-        const product = await getProductById(params.productId);
+        const product = await getProductById(productId);
 
         if (!product) {
             return NextResponse.json(
